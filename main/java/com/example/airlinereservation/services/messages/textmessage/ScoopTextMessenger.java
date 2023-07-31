3029adf6a551af8609a2eac2993181879c1df5e4
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-07-31 21:22:20 +0100

    new commit message

--- main/java/com/example/airlinereservation/services/messages/textmessage/ScoopTextMessenger.java
+++ main/java/com/example/airlinereservation/services/messages/textmessage/ScoopTextMessenger.java
@@ -5,18 +5,14 @@ import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.type.PhoneNumber;
 
 public class ScoopTextMessenger {
-	public static final String ACCOUNT_SID = "ACa7fee56d0b823400a73c0b2f47a8c336";
-	public static final String ACCOUNT_AUTH_TOKEN = "21a8ce5b060a202e8511c20c9241d381";
 	public static final String PHONE_NUMBER = "+14068047139";
 	public static void main(String[] args) {
-		Twilio.init(ACCOUNT_SID, ACCOUNT_AUTH_TOKEN);
+		Twilio.init(System.getenv("TWILIO_ACCOUNT_SID"), System.getenv("TWILIO_AUTH_TOKEN"));
 		MessageCreator messageCreator = Message.creator(new PhoneNumber("+2349061228207"), new PhoneNumber(PHONE_NUMBER),
 				"Hello First Message");
 		Message createdMessage = messageCreator.create();
 		System.out.println("Account Sid Is: "+createdMessage.getAccountSid());
 		System.out.println("Sid is: "+createdMessage.getSid());
 		System.out.println(System.getenv());
-		System.out.println(System.getenv("TWILIO_ACCOUNT_SID"));
-		System.out.println(System.getenv("TWILIO_AUTH_TOKEN"));
 	}
 }

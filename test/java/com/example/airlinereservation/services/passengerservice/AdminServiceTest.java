4cb00c00c24aebf67d9931715a0b49f1e860d1f5
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-08-06 17:29:56 +0100

    new commits

---
+++ test/java/com/example/airlinereservation/services/passengerservice/AdminServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.airlinereservation.services.passengerservice;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminServiceTest {
+	
+	@BeforeEach
+	void setUp() {
+	}
+	
+	@AfterEach
+	void tearDown() {
+	}
+}

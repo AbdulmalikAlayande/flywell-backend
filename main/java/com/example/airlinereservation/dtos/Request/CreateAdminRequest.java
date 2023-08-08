b86b0edf369a65e9f7996390b0fb40972ff4e9bd
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-08-09 00:58:03 +0100

    initial commit

--- main/java/com/example/airlinereservation/dtos/Request/CreateAdminRequest.java
+++ main/java/com/example/airlinereservation/dtos/Request/CreateAdminRequest.java
@@ -2,8 +2,7 @@ package com.example.airlinereservation.dtos.Request;
 
 import com.example.airlinereservation.data.model.UserBioData;
 
-public class CreateAdminRequest extends UserBioData {
-
-
+public class CreateAdminRequest {
+	
 
 }

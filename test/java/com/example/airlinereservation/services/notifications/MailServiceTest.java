c6c1fbac6ab07c182dc57876867957bb1a39600b
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-08-10 23:57:33 +0100

    wrote the implementation for the activation email and restructured the project

--- test/java/com/example/airlinereservation/services/notifications/MailServiceTest.java
+++ test/java/com/example/airlinereservation/services/notifications/MailServiceTest.java
@@ -4,6 +4,7 @@ import com.example.airlinereservation.dtos.Request.NotificationRequest;
 import com.example.airlinereservation.dtos.Response.NotificationResponse;
 import com.example.airlinereservation.services.notifications.mail.MailService;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,12 +17,17 @@ public class MailServiceTest {
 	
 	@Autowired
 	private MailService mailService;
+	private NotificationRequest notificationRequest;
+	@BeforeEach void beforeEachTestStartWith(){
+		notificationRequest = new NotificationRequest();
+		notificationRequest.setEmail("alayandezainab64@gmail.com");
+		notificationRequest.setUsername("Zee");
+	}
+	
 	
 	@SneakyThrows
 	@Test void sendEmailTest(){
-		NotificationRequest notificationRequest = new NotificationRequest();
-		notificationRequest.setEmail("alayandezainab64@gmail.com");
-		notificationRequest.setUsername("Zee");
+	
 		ResponseEntity<NotificationResponse> response = mailService.sendAccountActivationEmail(notificationRequest);
 		assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
 		assertThat(response.getBody()).isNotNull();
@@ -32,6 +38,6 @@ public class MailServiceTest {
 	}
 	
 	@Test void importContactTest(){
-		
+		mailService.importContacts(notificationRequest);
 	}
 }

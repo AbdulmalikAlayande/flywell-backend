f807c2068223cb1afcde33e6627acb1cc00ec72e
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-08-12 17:45:07 +0100

    wrote the implementation for the activation email and restructured the project

--- test/java/com/example/airlinereservation/services/flightservice/AirCraftServiceTest.java
+++ test/java/com/example/airlinereservation/services/flightservice/AirCraftServiceTest.java
@@ -1,9 +1,14 @@
 package com.example.airlinereservation.services.flightservice;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AirCraftServiceTest {
 
-
-
+	@Test void addAirCraftTest(){
+	
+	}
+	
+	
 }

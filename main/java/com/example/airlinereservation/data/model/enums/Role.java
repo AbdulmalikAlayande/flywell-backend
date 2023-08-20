94e620622112e5bb30fdbb2c81f832a2769504a3
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-08-21 00:27:40 +0100

    am back

--- main/java/com/example/airlinereservation/data/model/enums/Role.java
+++ main/java/com/example/airlinereservation/data/model/enums/Role.java
@@ -1,16 +1,14 @@
 package com.example.airlinereservation.data.model.enums;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@AllArgsConstructor
 public enum Role {
 	
 	ADMIN,
 	CREW_MEMBER("pilot", "co-pilot", "flight attendant"),
 	USER;
 	
-	Role(String role1, String role2, String role3) {
+	Role(String... roles) {
 	}
 }

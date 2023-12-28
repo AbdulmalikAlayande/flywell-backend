b9fd5d5d8e88ced57f961d9be2859b6ccb272a8d
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-12-28 14:53:33 +0100

    comms

--- src/main/java/com/example/airlinereservation/data/repositories/FlightRepository.java
+++ src/main/java/com/example/airlinereservation/data/repositories/FlightRepository.java
@@ -4,9 +4,12 @@ import com.example.airlinereservation.data.model.flight.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+
+@RepositoryRestResource(path = "flights")
 public interface FlightRepository extends JpaRepository<Flight, String> {
 	
 	
--- src/main/java/com/example/airlinereservation/data/repositories/UserBioDataRepository.java
+++ src/main/java/com/example/airlinereservation/data/repositories/UserBioDataRepository.java
@@ -2,9 +2,11 @@ package com.example.airlinereservation.data.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.airlinereservation.data.model.persons.UserBioData;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(path = "userbiodatas", collectionResourceRel = "userbiodatas", itemResourceRel = "userbiodata")
 public interface UserBioDataRepository extends JpaRepository<UserBioData, String> {
 	
 	

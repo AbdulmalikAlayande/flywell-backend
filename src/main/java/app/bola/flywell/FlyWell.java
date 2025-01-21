bbd5eeda0d56ce462f5503d8c8bdf93262c1b042
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2025-01-21 05:40:53 -0500

    deployed to heroku

--- src/main/java/app/bola/flywell/FlyWell.java
+++ src/main/java/app/bola/flywell/FlyWell.java
@@ -4,25 +4,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.pulsar.annotation.EnablePulsar;
+import org.springframework.web.bind.annotation.*;
 
 //@EnablePulsar
 @EnableJpaAuditing
 @SpringBootApplication
 public class FlyWell {
 
-		public int[] searchRange(int[] nums, int target) {
-			int[] newList = new int[2];
-			int newListCounter = 0;
-			for(int i = 0; i < nums.length; i++) {
-				if (target == nums[i]){
-					newList[newListCounter] = i;
-					newListCounter++;
-				}
+	public int[] searchRange(int[] nums, int target) {
+		int[] newList = new int[2];
+		int newListCounter = 0;
+		for(int i = 0; i < nums.length; i++) {
+			if (target == nums[i]){
+				newList[newListCounter] = i;
+				newListCounter++;
 			}
-			return newList;
 		}
-		
-		
+		return newList;
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(FlyWell.class, args);
 	}

d39f6b9cc18aacae2843138dec72ef27fc213d82
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2025-01-21 06:05:19 -0500

    deployed to heroku

--- src/main/java/app/bola/flywell/FlyWell.java
+++ src/main/java/app/bola/flywell/FlyWell.java
@@ -3,8 +3,8 @@ package app.bola.flywell;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.pulsar.annotation.EnablePulsar;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 //@EnablePulsar
 @EnableJpaAuditing
@@ -28,7 +28,6 @@ public class FlyWell {
 	String home() {
 		return "Hello World!";
 	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlyWell.class, args);

07c01986a2ff207b5136e6124766c6719390488d
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2025-01-21 13:49:44 -0500

    deployed to heroku

--- src/main/java/app/bola/flywell/config/EmailConfig.java
+++ src/main/java/app/bola/flywell/config/EmailConfig.java
@@ -4,7 +4,6 @@ import app.bola.flywell.services.notifications.FieldValidator;
 import app.bola.flywell.services.notifications.Validator;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -18,62 +17,58 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import java.util.Collections;
 
-
-@Configuration
-@EnableAutoConfiguration
 @Getter
+@Configuration
 public class EmailConfig {
-	public static final String EMAIL_TEMPLATE_ENCODING = "UTF-8";
-	
-	@Value("${totp.secret.key}")
-	private String totpSecret;
-	
-	@Value("${mail.api.key}")
-	private String brevoApiKey;
-	
-	@Bean
-	public ResourceBundleMessageSource emailMessageSource() {
-		final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("mail/MailMessages");
-		return messageSource;
-	}
-	@Bean
-	public TemplateEngine emailTemplateEngine() {
-		final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.addTemplateResolver(templateResolver());
-		templateEngine.setTemplateEngineMessageSource(emailMessageSource());
-		return templateEngine;
-	}
-	
-	private ITemplateResolver templateResolver(){
-		final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-		templateResolver.setOrder(2);
-		templateResolver.setResolvablePatterns(Collections.singleton("html/*"));
-		templateResolver.setPrefix("/mail/");
-		templateResolver.setSuffix(".html");
-		templateResolver.setTemplateMode(TemplateMode.HTML);
-		templateResolver.setCharacterEncoding(EMAIL_TEMPLATE_ENCODING);
-		templateResolver.setCacheable(false);
-		return templateResolver;
-	}
 
-	@Bean
-	public Context context() {
-	return new Context();
-	}
-	
-	@Bean
-	public Validator getValidator(){
-		return new FieldValidator();
-	}
-	@Bean
-	public EmailConfig emailConfig(){
-		return new EmailConfig();
-	}
-	
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
-	
+    public static final String EMAIL_TEMPLATE_ENCODING = "UTF-8";
+
+    @Value("${totp.secret.key}")
+    private String totpSecret;
+
+    @Value("${mail.api.key}")
+    private String brevoApiKey;
+
+    @Bean
+    public ResourceBundleMessageSource emailMessageSource() {
+        final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("mail/MailMessages");
+        return messageSource;
+    }
+    @Bean
+    public TemplateEngine emailTemplateEngine() {
+        final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(templateResolver());
+        templateEngine.setTemplateEngineMessageSource(emailMessageSource());
+        return templateEngine;
+    }
+
+    private ITemplateResolver templateResolver(){
+        final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setOrder(2);
+        templateResolver.setResolvablePatterns(Collections.singleton("html/*"));
+        templateResolver.setPrefix("/mail/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding(EMAIL_TEMPLATE_ENCODING);
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+
+    @Bean
+    public Context context() {
+        return new Context();
+    }
+
+    @Bean
+    public Validator getValidator(){
+        return new FieldValidator();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+
 }

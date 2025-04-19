dbf6d6a896e38e6d65b5f658a5e805898d79dbec
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2025-04-19 17:02:56 +0100

    Been Dealing with Cors error since the last ten (10) commits, so most of those commits with 'Trying to host paystackjava with JitPack' are actually having incorrect commit messages

---
+++ src/main/java/app/bola/flywell/security/filters/LastResortCorsFilter.java
@@ -0,0 +1,49 @@
+package app.bola.flywell.security.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class LastResortCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        String origin = request.getHeader("Origin");
+        if (origin != null && (
+                origin.contains("www.flywell.tech") ||
+                origin.contains("flywell.tech") ||
+                origin.contains("localhost:3000") ||
+                origin.contains("flywell.vercel.app"))
+        ) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void destroy() {}
+}

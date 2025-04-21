55b786a0be4f3d2f870531ae110317b472f8c6b4
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2025-04-21 16:57:36 +0100

    Been Dealing with Cors error since the last ten (10) commits, so most of those commits with 'Trying to host paystackjava with JitPack' are actually having incorrect commit messages

---
+++ src/main/java/app/bola/flywell/broker/FlyWellEventConsumer.java
@@ -0,0 +1,22 @@
+package app.bola.flywell.broker;
+
+import app.bola.flywell.data.model.users.User;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.client.api.SubscriptionType;
+import org.springframework.pulsar.annotation.PulsarListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FlyWellEventConsumer {
+
+
+    @PulsarListener(
+            topics = {"${spring.pulsar.producer.topic-name}"},
+            subscriptionName = "${spring.pulsar.consumer.subscription.name}",
+            subscriptionType = SubscriptionType.Key_Shared
+    )
+    public void consumePlainMessage(String message){
+        log.info("{}::handleCustomerMessage:[Consumed Event Customer:: {}]", getClass().getSimpleName(), message);
+    }
+}
--- src/main/java/app/bola/flywell/broker/FlyWellEventPublisher.java
+++ src/main/java/app/bola/flywell/broker/FlyWellEventPublisher.java
@@ -1,18 +1,14 @@
 package app.bola.flywell.broker;
 
-import app.bola.flywell.data.model.users.User;
 import app.bola.flywell.dto.response.CustomerResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.pulsar.core.PulsarTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class FlyWellPublisher {
+public class FlyWellEventPublisher {
 
 //    final PulsarTemplate<User> pulsarTemplate;
 //    final ModelMapper mapper;
